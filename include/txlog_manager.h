e595a0486c75845dc37184f6837ae9408eb210cb
Shohei Yamamoto <ymt@souffle>
2023-09-25 15:18:56 +0900

    add tx log manager

--- include/txlog_manager.h
+++ include/txlog_manager.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <stdio.h>
+#include <pthread.h>
 
 struct txlm_config_t
 {
-    FILE *fp;
     char filename[16];
+    pthread_mutex_t mutex;
     size_t latest_lsn;
 };
 typedef struct txlm_config_t txlm_config_t;
@@ -13,22 +14,22 @@ typedef struct txlm_config_t txlm_config_t;
 struct txlog_t
 {
     /* txlog header */
-    size_t lsn;
-    size_t client_id;
+    unsigned int lsn;
+    short client_id;
     double add_time;
-    size_t log_size;
+    unsigned int data_size;
 };
 typedef struct txlog_t txlog_t;
 
 #define TXLOG_HEADER_OFFSET_LSN         0
-#define TXLOG_HEADER_OFFSET_CLIENTID    TXLOG_HEADER_OFFSET_LSN+sizeof(size_t)
-#define TXLOG_HEADER_OFFSET_ADDTIME     TXLOG_HEADER_OFFSET_LSN+sizeof(size_t)
-#define TXLOG_HEADER_OFFSET_LOGSIZE     TXLOG_HEADER_OFFSET_ADDTIME+sizeof(double)
-#define TXLOG_HEADER_SIZE               TXLOG_HEADER_OFFSET_LOGSIZE+sizeof(size_t)
+#define TXLOG_HEADER_OFFSET_CLIENTID    TXLOG_HEADER_OFFSET_LSN+sizeof(unsigned int)
+#define TXLOG_HEADER_OFFSET_ADDTIME     TXLOG_HEADER_OFFSET_LSN+sizeof(short)
+#define TXLOG_HEADER_OFFSET_DATASIZE    TXLOG_HEADER_OFFSET_ADDTIME+sizeof(double)
+#define TXLOG_HEADER_SIZE               TXLOG_HEADER_OFFSET_DATASIZE+sizeof(unsigned int)
 
 txlm_config_t *txlm_init(size_t server_id);
 void txlm_deinit(txlm_config_t *txlm_config);
-size_t txlm_append_log(txlm_config_t *txlm_config, size_t client_id, double add_time, size_t send_log_size);
-void txlm_read_log(txlm_config_t *txlm_config, size_t lsn, txlog_t *log);
-void txlm_wirte_log(txlm_config_t *txlm_config, txlog_t *log);
-void txlm_bytes_to_log(txlog_t *log, char *bytes, size_t len);
+size_t txlm_append_log(txlm_config_t *txlm_config, unsigned short client_id, double add_time, unsigned int data_size);
+void txlm_read_log(unsigned char *bytes, txlm_config_t *txlm_config, size_t lsn);
+void txlm_wirte_log(txlm_config_t *txlm_config, size_t lsn, unsigned char *bytes, size_t log_size);
+void txlm_get_info_from_header(txlog_t *log, char *bytes, size_t len);
--- src/txlog_manager.c
+++ src/txlog_manager.c
@@ -1,44 +1,83 @@
+#include <errno.h>
 #include <stdlib.h>
 
 #include "txlog_manager.h"
+#include "server.h"
 
-lm_config_t *lm_init(size_t server_id)
+txlm_config_t *txlm_init(size_t server_id)
 {
-    lm_config_t *lm_config = malloc(sizeof(lm_config_t));
-    sprintf(lm_config->filename, "txlog_srv%d", server_id);
-    lm_config->latest_lsn = 0;
+    txlm_config_t *txlm_config = malloc(sizeof(txlm_config_t));
 
-    lm_config->fp = fopen(lm_config->filename, "wb+");
-    if (lm_config->fp == NULL) {
-        fprintf(stderr, "log_manager.c: (line:%d) %s", __LINE__, strerror(errno));
-        exit(1);
-    }
+    sprintf(txlm_config->filename, "txlog_srv%d.bin", server_id);
+    remove(txlm_config->filename);
 
-    return lm_config;
+    pthread_mutex_init(&txlm_config->mutex, NULL);
+
+    txlm_config->latest_lsn = 0;
+
+    return txlm_config;
 }
 
-void lm_deinit(lm_config_t *lm_config)
+void txlm_deinit(txlm_config_t *txlm_config)
 {
-    fclose(lm_config->fp);
-    free(lm_config);
+    pthread_mutex_destroy(&txlm_config->mutex);
+    free(txlm_config);
 }
 
-size_t lm_append_log(lm_config_t *lm_config, size_t client_id, double add_time, size_t send_log_size)
+size_t txlm_append_log(txlm_config_t *txlm_config, unsigned short client_id, double add_time, unsigned int log_size)
 {
+    FILE *fp;
+    unsigned char bytes[log_size+1];
+    unsigned int lsn;
+    double add_time;
+    unsigned int data_size;
+    size_t offset;
 
+    memcpy(bytes + TXLOG_HEADER_OFFSET_LSN, &lsn, sizeof(unsigned int));
+    memcpy(bytes + TXLOG_HEADER_OFFSET_CLIENTID, &client_id, sizeof(unsigned int));
+    memcpy(bytes + TXLOG_HEADER_OFFSET_ADDTIME, &add_time, sizeof(double));
+    memcpy(bytes + TXLOG_HEADER_OFFSET_DATASIZE, &data_size);
+
+
+    pthread_mutex_lock(&txlm_config->mutex);
+
+    fp = fopen(txlm_config->filename, "wb");
+    fseek(fp, offset, SEEK_SET);
+    fwrite(bytes, sizeof(unsigned char), log_size, fp);
+
+    pthread_mutex_unlock(&txlm_config->mutex);
 }
 
-void lm_read_log(lm_config_t *lm_config, size_t lsn, log_t *log)
+void txlm_read_log(unsigned char *bytes, txlm_config_t *txlm_config, size_t lsn)
 {
+    FILE *fp;
+    size_t offset;
+    size_t log_size;
+    
+    pthread_mutex_lock(&txlm_config->mutex);
 
+    fp = fopen(txlm_config->filename, "rb");
+    fseek(fp, offset, SEEK_SET);
+    fread(bytes, sizeof(unsigned char), log_size, fp);
+
+    pthread_mutex_unlock(&txlm_config->mutex);
 }
 
-void lm_wirte_log(lm_config_t *lm_config, log_t *log)
+void txlm_wirte_log(txlm_config_t *txlm_config, size_t lsn, unsigned char *bytes, size_t log_size)
 {
+    FILE *fp;
+    size_t offset;
+    
+    pthread_mutex_lock(&txlm_config->mutex);
 
+    fp = fopen(txlm_config->filename, "wb");
+    fseek(fp, offset, SEEK_SET);
+    fwrite(bytes, sizeof(unsigned char), log_size, fp);
+
+    pthread_mutex_unlock(&txlm_config->mutex);
 }
 
-void lm_bytes_to_log(log_t *log, char *bytes, size_t len)
+void txlm_get_info_from_header(txlog_t *log, char *bytes, size_t len)
 {
 
 }
