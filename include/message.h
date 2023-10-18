77907061090da7c8c00e0a953e2dea014a424463
Shohei Yamamoto <mf22135@shibaura-it.ac.jp>
2023-10-19 00:01:05 +0900

    message typeの定義

--- include/message.h
+++ include/message.h
@@ -1,1 +1,12 @@
 #pragma once
+
+enum message_enum
+{
+    TXLOGDATA_MESSAGE,
+    ACK_MESSAGE,
+    ERROR,
+};
+typedef enum message_enum message_e;
+
+#define MESSAGE_HEADER_OFFSET_SIZE      0
+#define MESSAGE_HEADER_OFFSET_TYPE      
