36fe75f381da3f5d92226e462faebb00cd813c45
Shohei Yamamoto <mf22135@shibaura-it.ac.jp>
2023-10-19 16:07:28 +0900

    ノード間通信メッセージのヘッダの管理

--- include/message.h
+++ include/message.h
@@ -2,11 +2,31 @@
 
 enum message_enum
 {
-    TXLOGDATA_MESSAGE,
-    ACK_MESSAGE,
+    TXLOG_MESSAGE,
+    TXLOGACK_MESSAGE,
+    FIN_MESSAGE,
     ERROR,
 };
-typedef enum message_enum message_e;
+typedef enum message_enum msg_e;
 
-#define MESSAGE_HEADER_OFFSET_SIZE      0
-#define MESSAGE_HEADER_OFFSET_TYPE      
+struct message_info_t
+{
+    msg_e type;
+    short source_id;
+    short destination_id;
+    unsigned int lsn_ack;
+};
+typedef struct message_info_t msg_info_t;
+
+/* ヘッダオフセット */
+#define MESSAGE_HEADER_OFFSET_TYPE              0
+#define MESSAGE_HEADER_OFFSET_SOURCEID          MESSAGE_HEADER_OFFSET_TYPE + sizeof(short)
+#define MESSAGE_HEADER_OFFSET_DESTINATIONID     MESSAGE_HEADER_OFFSET_SOURCEID + sizeof(short)
+#define MESSAGE_HEADER_OFFSET_LSNACK            MESSAGE_HEADER_OFFSET_DESTINATIONID + sizeof(short)
+#define MESSAGE_HEADER_SIZE                     MESSAGE_HEADER_OFFSET_LSNACK + sizeof(unsigned int)
+
+
+void get_info_from_message_header(char *msg, msg_info_t *msg_info);
+unsigned int create_txlog_message_header(char *msg, short source_id, short destination_id);
+unsigned int create_txlogack_message_header(char *msg, short source_id, short destination_id, unsigned int lsn_ack);
+unsigned int create_fin_message_header(char *msg, short source_id, short destination_id);
--- src/message.c
+++ src/message.c
@@ -1,1 +1,47 @@
-#include "manager.h"
+#include <string.h>
+#include <stdio.h>
+
+#include "message.h"
+
+void get_info_from_message_header(char *msg, msg_info_t *msg_info)
+{
+    memcpy(&msg_info->type, msg + MESSAGE_HEADER_OFFSET_TYPE, sizeof(short));
+    memcpy(&msg_info->source_id, msg + MESSAGE_HEADER_OFFSET_SOURCEID, sizeof(short));
+    memcpy(&msg_info->destination_id, msg + MESSAGE_HEADER_OFFSET_DESTINATIONID, sizeof(short));
+    memcpy(&msg_info->lsn_ack, msg + MESSAGE_HEADER_OFFSET_LSNACK, sizeof(unsigned int));
+}
+
+unsigned int create_txlog_message_header(char *msg, short source_id, short destination_id)
+{
+    msg_e type = TXLOG_MESSAGE;
+    unsigned int lsn_ack = -1;
+
+    memcpy(msg + MESSAGE_HEADER_OFFSET_TYPE, &type, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_SOURCEID, &source_id, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_DESTINATIONID, &destination_id, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_LSNACK, &lsn_ack, sizeof(unsigned int));
+    return MESSAGE_HEADER_SIZE;
+}
+
+unsigned int create_txlogack_message_header(char *msg, short source_id, short destination_id, unsigned int lsn_ack)
+{
+    msg_e type = TXLOGACK_MESSAGE;
+
+    memcpy(msg + MESSAGE_HEADER_OFFSET_TYPE, &type, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_SOURCEID, &source_id, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_DESTINATIONID, &destination_id, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_LSNACK, &lsn_ack, sizeof(unsigned int));
+    return MESSAGE_HEADER_SIZE;
+}
+
+unsigned int create_fin_message_header(char *msg, short source_id, short destination_id)
+{
+    msg_e type = FIN_MESSAGE;
+    unsigned int lsn_ack = -1;
+
+    memcpy(msg + MESSAGE_HEADER_OFFSET_TYPE, &type, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_SOURCEID, &source_id, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_DESTINATIONID, &destination_id, sizeof(short));
+    memcpy(msg + MESSAGE_HEADER_OFFSET_LSNACK, &lsn_ack, sizeof(unsigned int));
+    return MESSAGE_HEADER_SIZE;
+}
