296a30c4c98218dd88f253c080616d5b6b6398c5
Shohei Yamamoto <mf22135@shibaura-it.ac.jp>
2024-10-08 19:12:06 +0900

    送信先情報に関するクラスのファイルを統合

--- endpoint.hpp
+++ endpoint.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 namespace multicast
 {
 class Endpoint
@@ -9,6 +11,26 @@ public:
     ~Endpoint();
 private:
     /* data */
+};
+
+class NeighborGroup
+{
+public:
+    NeighborGroup(/* args */);
+    ~NeighborGroup();
+    void add_node();
+private:
+    std::vector<multicast::Endpoint> nodes;
+};
+
+class MulticastGroup
+{
+public:
+    MulticastGroup(/* args */);
+    ~MulticastGroup();
+    void add_node();
+private:
+    std::vector<NeighborGroup> groups;
 };
 
 } // namespace multicast
