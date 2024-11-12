772beb71495e51211ec47e19d9688548b8ea46e4
Shohei Yamamoto <mf22135@shibaura-it.ac.jp>
2024-11-12 16:39:39 +0900

    修正

--- sample/receiver.cpp
+++ sample/receiver.cpp
@@ -7,13 +7,15 @@
 
 int main(int argc, char *argv[]) {
     uint16_t id = stoi(std::string(argv[1]));
+    int len;
     char buf[256];
     multicast::Socket sock = multicast::Socket();
 
     sock.open(SRC_PORT + id);
 
     for (;;) {
-        sock.recvfrom(buf);
+        len = sock.recvfrom(buf);
+        buf[len] = '\0';
         std::cout << "recvfrom: " << buf << std::endl;
     }
     sock.close();
