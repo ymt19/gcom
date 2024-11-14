5962d7df8d76b5369629ee6812de377595744199
Shohei Yamamoto <mf22135@shibaura-it.ac.jp>
2024-11-14 21:21:36 +0900

    todoリスト

---
+++ sample/run.py
@@ -0,0 +1,11 @@
+# 引数
+## data/sec [byte/msec]
+## ロス率，遅延
+## senderのIPアドレス　ポート
+## receiverの数
+## reciever1 ipaddr port
+## reciever2 ipaddr port
+## .....
+
+# docker-composeコード生成
+# docker-compose実行
