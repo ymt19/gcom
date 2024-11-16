eaa42a0edbadb9fe0000873b026221d539fb8e5d
Shohei Yamamoto <mf22135@shibaura-it.ac.jp>
2024-11-16 18:10:58 +0900

    docker composeのコード生成

--- sample/gen-docker-compose.py
+++ sample/gen-docker-compose.py
@@ -12,16 +12,33 @@
 # docker-composeコード生成
 from jinja2 import Template
 from yaml import safe_load
+import datetime
+import sys
 
-with open("config.yml") as f:
+args = sys.argv
+
+# YAML形式の設定ファイルを開く
+with open(args[1]) as f:
     data = safe_load(f)
 
-mnt_dir = "/usr/src/sample/"
-working_dir = "" # mnt_dir/241116-000000-0%-0ms
-volumes = ".:/usr/src/sample"
-ipaddr = "192.168.0.0/24"
+# 現在時刻の取得
+dt_now = datetime.datetime.now()
+dt_str = dt_now.strftime('%y%m%d-%H%M%S')
 
-print(data)
+# 設定値の計算
+mnt_dir = "/usr/src/sample/"
+working_dir = '{}{}-{}%-{}ms'.format(mnt_dir, dt_str, data['network']['loss_rate'], data['network']['delay']) # mnt_dir/20241116-000000-0%-0ms
+volumes = ".:{}".format(mnt_dir)
+subnet = "192.168.0.0/24"
+port = 10000
+
+sender = {'name': 'sender', 'ipaddr': subnet.replace('0/24', str(10 + 1)), 'port': port}
+receivers = []
+for i in range(data['application']['replicas']):
+    receivers.insert(i, {})
+    receivers[i]['name'] = 'receiver{}'.format(i)
+    receivers[i]['ipaddr'] = subnet.replace("0/24", str(20 + i))
+    receivers[i]['port'] = port
 
 template = Template('''
 version: \'3\'
@@ -33,18 +50,32 @@ services:
             - {{volumes}}
         networks:
             intra_net:
-                ipv4_address: {{ipaddr}}
+                ipv4_address: {{sender['ipaddr']}}
         cap_add:
             - NET_ADMIN
         depends_on:
             - {{receiver}}
+
+    {% for receiver in receivers %}
+    {{receiver['name']}}
+        image: sample
+        working_dir: {{working_dir}}
+        volumes:
+            - {{volumes}}
+        networks:
+            intra_net:
+                ipv4_address: {{receiver['ipaddr']}}
+        cap_add:
+            - NET_ADMIN
+    {% endfor %}
 
 networks:
     intra_net:
         ipam:
             driver: default
             confg:
-                - subnet: {{ipaddr}}
+                - subnet: {{subnet}}
 ''')
 
-print(template.render(working_dir=working_dir, volumes=volumes, ipaddr=ipaddr,receiver='receiver'))
+output = template.render(working_dir=working_dir, volumes=volumes, sender=sender, receivers=receivers, subnet=subnet)
+print(output)
