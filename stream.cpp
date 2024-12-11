13164ab6dfb692c27befa5963e0ef424871c2809
Shohei Yamamoto <mf22135@shibaura-it.ac.jp>
2024-12-11 23:02:13 +0900

    class stream

--- stream.cpp
+++ stream.cpp
@@ -1,87 +1,6 @@
 #include "stream.hpp"
 
-void gcom::stream::push_packets(unsigned char *data, uint32_t len, uint32_t max_payload_size)
-{
-    uint32_t idx, head_idx, tail_idx;
-
-    std::lock_guard<std::mutex> lock(mtx);
-
-    head_idx = buff.get_write_idx();
-    tail_idx = head_idx + len;
-    idx = head_idx;
-
-    // 分割して挿入
-    while(idx != tail_idx)
-    {
-        uint32_t payload_size = std::min(max_payload_size, tail_idx - head_idx);
-        uint64_t idx = buff.push(data + (idx - head_idx), payload_size);
-        info.emplace(idx, packet(payload_size, head_idx, tail_idx));
-
-        idx += payload_size;
-    }
-
-    // zombie index未満のidxに格納されるパケット情報をinfoから削除
-    auto itr = info.begin();
-    while (itr != info.end())
-    {
-        if (buff.get_zombie_idx() <= itr->first)
-        {
-            break;
-        }
-        itr = info.erase(itr);
-    }
-}
-
-void gcom::stream::insert_packet(unsigned char *data, uint32_t len, uint32_t idx, uint32_t head_idx, uint32_t tail_idx)
-{
-    std::lock_guard<std::mutex> lock(mtx);
-
-    if (tail_idx > buff.get_write_idx())
-    {
-        buff.push_empty(tail_idx - buff.get_write_idx());
-    }
-
-    buff.set(idx, data, len);
-    info.emplace(idx, packet(len, head_idx, tail_idx));
-
-    if (next_idx == idx)
-    {
-        next_idx += len;
-        auto itr = info.find(next_idx);
-        while (itr != info.end())
-        {
-            if (next_idx == itr->first)
-            {
-                next_idx += itr->second.payload_size;
-                itr++;
-            } else
-            {
-                break;
-            }
-        }
-    }
-}
-
-uint32_t gcom::stream::pop_packets(unsigned char *data)
-{
-    uint64_t read_idx, len;
-
-    std::lock_guard<std::mutex> lock(mtx);
-
-    read_idx = buff.get_read_idx();
-    len = info.at(read_idx).tail_idx - info.at(read_idx).head_idx;
-    if (next_idx > read_idx + len)
-    {
-        buff.get(read_idx, data, len);
-        return len;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-uint32_t gcom::stream::get_packet(uint32_t idx, unsigned char *data, uint32_t *head_idx, uint32_t *tail_idx)
+uint32_t gcom::stream::get_packet(uint32_t idx, unsigned char *payload, uint32_t *tail_idx)
 {
     std::lock_guard<std::mutex> lock(mtx);
 
@@ -95,9 +14,8 @@ uint32_t gcom::stream::get_packet(uint32_t idx, unsigned char *data, uint32_t *h
     {
         throw std::exception();
     }
-    
-    *head_idx = itr->second.head_idx;
+
     *tail_idx = itr->second.tail_idx;
-    buff.get(idx, data, itr->second.payload_size);
+    buff.get(idx, payload, itr->second.payload_size);
     return itr->second.payload_size;
 }
--- stream.hpp
+++ stream.hpp
@@ -8,50 +8,29 @@
 namespace gcom
 {
 
-class stream
-{
-public:
-    stream(int buffsize)
-        : buff(buffsize), next_idx(0) {}
-
-    /**
-     * @brief データをパケットに分割してbuffに挿入（sender）
-     * @note
-     * 上書きにより削除されたデータをinfoから削除
-     * nextseqの修正
-     */
-    void push_packets(unsigned char *data, uint32_t len, uint32_t max_payload_size);
-
-    /**
-     * @brief buffにデータを挿入（receiver）
-     */
-    void insert_packet(unsigned char *data, uint32_t len, uint32_t idx, uint32_t head_idx, uint32_t tail_idx);
-
-    /**
-     * @brief buffからデータを削除（sender/receiver）
-     */
-    uint32_t pop_packets(unsigned char *data);
-
-    /**
-     * @brief buffからindexのデータを取得（sender/receiver）
-     */
-    uint32_t get_packet(uint32_t idx, unsigned char *data, uint32_t *head_idx, uint32_t *tail_idx);
-private:
-    class packet
+    class stream
     {
     public:
-        packet(uint32_t payload_size, uint32_t head_idx, uint32_t tail_idx)
-            : payload_size(payload_size), head_idx(head_idx), tail_idx(tail_idx) {}
+        stream(int buffsize) : buff(buffsize) {}
 
-        uint32_t payload_size;
-        uint32_t head_idx;
-        uint32_t tail_idx;
+        /**
+         * @brief buffからindexのデータを取得
+         */
+        uint32_t get_packet(uint32_t idx, unsigned char *payload, uint32_t *tail_idx);
+
+        class packet_entry
+        {
+        public:
+            packet_entry(uint32_t payload_size, uint32_t tail_idx)
+                : payload_size(payload_size), tail_idx(tail_idx) {}
+
+            uint32_t payload_size;
+            uint32_t tail_idx;
+        };
+
+        std::mutex mtx;
+        ring_buffer buff;
+        std::map<uint64_t, packet_entry> info;    // idx : packet_entry
     };
-
-    std::mutex mtx;
-    ring_buffer buff;
-    std::map<uint64_t, packet> info;    // idx : packet
-    uint32_t next_idx; // only receiver
-};
 
 } // namespace gcom
